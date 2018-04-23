7234699c4fa14f6397400e18992d11068b5c081b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GaugePropertyEditor.Win/Module.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+
+namespace GaugePropertyEditor.Win {
+    public sealed partial class GaugePropertyEditorWindowsFormsModule : ModuleBase {
+        public GaugePropertyEditorWindowsFormsModule() {
+            InitializeComponent();
+        }
+    }
+}
